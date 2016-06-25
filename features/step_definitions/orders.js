80e3b045abb2469a1c16817076d663821318f494
gustavoruas <fabriziobrusca87@gmail.com>
2016-06-25 12:36:02 +0000

    versao 2

--- features/step_definitions/orders.js
+++ features/step_definitions/orders.js
@@ -48,44 +48,5 @@ module.exports = function () {
     
     
     //******Adicionando cenario 2
-   this.Given(/^a valid order$/, function (status) {
-        const 
-            that = this,
-            payload = {
-            data: {
-                type: 'orders',
-                attributes: {
-                    status: status,
-                    items: [{ product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 1}]
-                    }
-                }
-            }
-        
-
-        return this.doHttpRequest('orders', 'post', payload)
-        .then((response) => {
-            that.existingOrder = response.body;
-            return response;
-        });
-    });
-    
-    this.When(/^I submit it to the API$/, function () {
-        const 
-            that = this,
-            id = this.existingOrder.data.id;
-        return this.doHttpRequest('orders/' + id, 'get', undefined)
-        .then((response) => {
-            that.responseBody = JSON.parse(response.body);
-            return response;
-        });
-    });
-    
-    this.Then(/^I receive a success message$/, function () {
-        expect(this.responseBody.data).not.to.be.undefined;
-    });
-    
-    this.Then(/^its status is (.*)$/, function (status) {
-        expect(this.responseBody.data.attributes.status).to.equal(status);
-    });
     
 }
